88b0c4c42692d089ee3dabdb47366b19f9ae7c32
Abdul Alim Baig <71715745+AbdulAlim8660@users.noreply.github.com>
2025-10-01 18:45:15 +0530

    updated test

--- monitor.py
+++ monitor.py
@@ -2,13 +2,14 @@ from utils import sleep_func
 
 
 def vitals_ok(temperature, pulseRate, spo2):
-    if temperature not in range(95, 102) or pulseRate not in range(60, 100) or spo2 < 90:
-        if temperature not in range(95, 102):
-            print('Temperature critical!')
-        if pulseRate not in range(60, 100):
-            print('Pulse Rate is out of range!')
-        if spo2 < 90:
-            print('Oxygen Saturation out of range!')
+    if temperature not in range(95,102):
+        print('Temperature critical!')
         sleep_func()
-        return False
-    return True
+    elif pulseRate not in range(60,100):
+        print('Pulse Rate is out of range!')
+        sleep_func()
+    elif spo2 < 90:
+        print('Oxygen Saturation out of range!')
+        sleep_func()
+    else:
+        return True
